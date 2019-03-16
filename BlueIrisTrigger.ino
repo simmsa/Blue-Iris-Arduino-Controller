88bc822338e3e74b227fd03da3a05d118df9d8c5
Andrew Simms <simms.andrew@gmail.com>
2019-03-16 15:38:55 -0600

    Feature: Try to trigger blue iris via `Serial`
    What inputs does blue iris accept? Here I am printing numbers.
    
    Do I need to turn blue iris off? Currently we do with `Serial.print(0)`

--- BlueIrisTrigger.ino
+++ BlueIrisTrigger.ino
@@ -1,9 +1,35 @@
-void setup() {
-  // put your setup code here, to run once:
+#define LED_PIN 13
+#define SWITCH_INPUT A0
+#define SPEAKER_PIN 9
 
+#define ON 1
+#define OFF 0
+
+bool switchPressed() {
+    return digtalRead(SWITCH_INPUT) == 0;
+}
+
+bool state = OFF;
+
+void setup() {
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(SPEAKER_PIN, OUTPUT);
+    pinMode(SWITCH_INPUT, INPUT);
+
+    Serial.begin(9600);
+    state = OFF;
+    
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    bool switchStatus = switchPressed();
+    if(switchStatus && state == OFF) {
+        Serial.print(2);
+        digitalWrite(LED_PIN, HIGH);
+        state = ON;
+    } else if (!switchStatus && state = ON){
+        digitalWrite(LED_PIN, LOW);
+        Serial.print(0);
+    }
 
 }
