3e4c25570df8959b6fa7ae8343677545ede5ded3
Andrew Simms <simms.andrew@gmail.com>
2019-03-19 19:16:17 -0600

    Feature: Add a sane way to trigger Blue Iris
    See the comment here

--- BlueIrisTrigger.ino
+++ BlueIrisTrigger.ino
@@ -1,3 +1,5 @@
+#include <math.h>
+
 #define LED_PIN 13
 #define SWITCH_INPUT A0
 #define SPEAKER_PIN 3
@@ -5,8 +7,18 @@
 #define ON 1
 #define OFF 0
 
-bool switchPressed() {
-    return digitalRead(SWITCH_INPUT) == 0;
+// Blue Iris accepts a number called "Global input dio bits" to trigger a
+// camera. This function converts the input number into a bitwise number,
+// the format Blue Iris expects (a set of bytes with with a 1 in the nth
+// position. Ex 6 = 0010000, 3 = 00000100, etc). The arduino's unsigned int type
+// has 4 bytes (32 bits) so highest input this function can handle is 32 before
+// it overflows
+void startBlueIrisRecording(unsigned int global_input_dio_bits) {
+    // This is a bitwise operation that converts the input number to a binary
+    // number with the 1 at the input digit.
+    // Ex: 6 -> 00100000
+    unsigned int bitwise_dio = pow(global_input_dio_bits, 2);
+    Serial.write(bitwise_dio);
 }
 
 void stopBlueIrisRecording() { Serial.write(0); }
